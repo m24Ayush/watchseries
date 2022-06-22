f2b242aa01d38973afff4b1511ddef62c18b6788
m24Ayush <105962137+m24Ayush@users.noreply.github.com>
2022-06-22 15:21:44 +0530

    Reboot

---
+++ DCAMU/dcamu.js
@@ -0,0 +1,30 @@
+// fetch data from json file and display all series card
+// fetch("item.json")
+fetch("dcamu.json")
+.then(response => response.json())
+.then(data => {
+    console.log(data);
+    
+    const list = document.querySelector('.list');
+    // console.log(list);
+    
+    // Display multiple series card using json file
+    // for (let i = 0; i < data.series.length; i++) {
+    data.forEach(element => {
+        
+        // creating a div element
+        const item = document.createElement('div');
+        // give it a class name
+        item.className = 'list-item';
+        
+        const listItem = `
+        <a href=${element.link}>
+        <img class="list-item-img" src=${element.image}>
+        <h2 class="list-item-title">${element.title}</h2>
+        </a>`;
+        
+        // adding html in the div tag
+        item.innerHTML += listItem;
+        list.appendChild(item);
+    });
+});
